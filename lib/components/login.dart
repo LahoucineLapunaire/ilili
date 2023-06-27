f9598d71c3bc5125ab03c4fc40bcb56ad4923aca
lahoucine.lapunaire <lahoucine.lapunaire@ensiie.fr>
2023-06-27 13:16:32 +0200

    [FEAT] Login : add login structur and logic, handling error OK

--- lib/components/login.dart
+++ lib/components/login.dart
@@ -7,39 +7,261 @@ FirebaseAuth auth = FirebaseAuth.instance;
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  void login() {
-    auth
-        .signInWithEmailAndPassword(email: "test@test.com", password: "testest")
-        .then((value) => print("User ${value.user?.email} logged in"));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/logo.png',
-          ),
-          ElevatedButton(
-              onPressed: () {
-                login();
-              },
-              child: Text('Login')),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SignupPage(), // Destination screen
-                  ),
-                );
-              },
-              child: Text('To Signup')),
-        ],
+          child: Container(
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Color(0xFF6A1B9A),
+            Color(0xFFCD7CFF),
+          ],
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft,
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LogoSection(),
+            SizedBox(height: 20),
+            TitleSection(),
+            SizedBox(height: 20),
+            FormSection(),
+            SizedBox(height: 20),
+            ToSignup(),
+            ToForgottedPassword(),
+          ],
+        ),
       )),
+    );
+  }
+}
+
+class LogoSection extends StatelessWidget {
+  LogoSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250,
+      height: 250,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        image: DecorationImage(
+          image: AssetImage('assets/images/logo.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("ilili",
+            style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.w900,
+                color: Colors.white)),
+        SizedBox(width: 10),
+        Text("login",
+            style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.w900,
+                color: Color(0xFF009688))),
+      ],
+    );
+  }
+}
+
+class FormSection extends StatefulWidget {
+  FormSection({super.key});
+
+  @override
+  _FormSectionState createState() => _FormSectionState();
+}
+
+class _FormSectionState extends State<FormSection> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool error = false;
+  String errorMessage = "";
+
+  void login() async {
+    if (emailController.text == '' || passwordController.text == '') {
+      setState(() {
+        error = true;
+        errorMessage = "All fields must be filled";
+      });
+      return;
+    }
+    try {
+      await auth
+          .signInWithEmailAndPassword(
+              email: emailController.text, password: passwordController.text)
+          .then((value) => {print("User ${value.user?.email} logged in")});
+    } catch (e) {
+      setState(() {
+        error = true;
+        errorMessage = e.toString().split('] ')[1];
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      child: Column(
+        children: [
+          if (error)
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Icon(Icons.error, color: Colors.red),
+                  SizedBox(width: 5),
+                  Container(
+                    width: 250,
+                    child: Text(
+                      "${errorMessage}",
+                      style: TextStyle(
+                          color: Colors.red, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          TextField(
+            controller: emailController,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              prefixIcon: Icon(Icons.email),
+              labelText: 'Email',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          TextField(
+            obscureText: true,
+            controller: passwordController,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              prefixIcon: Icon(Icons.lock),
+              labelText: 'Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              login();
+            },
+            child: Text('Login'),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              fixedSize:
+                  Size(200, 50), // Set the width and height of the button
+              backgroundColor:
+                  Color(0xFF009688), // Set the background color of the button
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ToSignup extends StatelessWidget {
+  const ToSignup({super.key});
+
+  void redirectToLogin(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignupPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        redirectToLogin(context);
+      },
+      child: Text("No account ? register now "),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(
+            Colors.transparent), // Set the background color to transparent
+        elevation: MaterialStateProperty.all<double>(
+            0), // Remove the button's elevation
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+                8), // Customize the button's border radius as desired
+            side: BorderSide(
+                color: Colors.transparent), // Remove the button's border
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ToForgottedPassword extends StatelessWidget {
+  const ToForgottedPassword({super.key});
+
+  void redirectToLogin(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignupPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        redirectToLogin(context);
+      },
+      child: Text("password forgotten ?"),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(
+            Colors.transparent), // Set the background color to transparent
+        elevation: MaterialStateProperty.all<double>(
+            0), // Remove the button's elevation
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+                8), // Customize the button's border radius as desired
+            side: BorderSide(
+                color: Colors.transparent), // Remove the button's border
+          ),
+        ),
+      ),
     );
   }
 }
