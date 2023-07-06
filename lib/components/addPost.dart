96cec02ad96f807d15a91f09854f6b68e93549b1
lahoucine.lapunaire <lahoucine.lapunaire@ensiie.fr>
2023-07-06 11:38:50 +0200

    [FIX] AddPost : audio recorder can now be play by audio player

--- lib/components/addPost.dart
+++ lib/components/addPost.dart
@@ -4,8 +4,11 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'dart:async';
+import 'dart:io';
 
 List<String> tagsList = [];
+AudioPlayer audioPlayer = AudioPlayer();
+String audioPath = '';
 
 class AddPostPage extends StatelessWidget {
   const AddPostPage({super.key});
@@ -17,14 +20,12 @@ class AddPostPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AudioPlayerWidget(
-                audioPath:
-                    'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'),
-            Divider(
-              height: 50,
-              thickness: 2,
-            ),
-            RecordSection(),
+            Text(
+                "To add a post, please record an audio file or upload one, and add some tags to it, and then click on the 'Add Post' button."),
+            SizedBox(height: 20),
+            ButtonSection(),
+            SizedBox(height: 20),
+            AudioPlayerSection(),
             Divider(
               height: 50,
               thickness: 2,
@@ -37,140 +38,17 @@ class AddPostPage extends StatelessWidget {
   }
 }
 
-class AudioPlayerWidget extends StatefulWidget {
-  final String audioPath;
-
-  AudioPlayerWidget({required this.audioPath});
+class ButtonSection extends StatefulWidget {
+  const ButtonSection({super.key});
 
   @override
-  _AudioPlayerWidgetState createState() => _AudioPlayerWidgetState();
+  State<ButtonSection> createState() => _ButtonSectionState();
 }
 
-class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
-  AudioPlayer _audioPlayer = AudioPlayer();
-  String _filePath = '';
-  bool _isPlaying = false;
-  Duration _audioDuration = Duration();
-  Duration _position = Duration();
-
-  @override
-  void initState() {
-    super.initState();
-    _audioPlayer.onDurationChanged.listen((Duration duration) {
-      setState(() {
-        _audioDuration = duration;
-      });
-    });
-
-    _audioPlayer.onPositionChanged.listen((Duration position) {
-      setState(() {
-        _position = position;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _audioPlayer.release();
-    super.dispose();
-  }
-
-  void _playPause() async {
-    if (_isPlaying) {
-      await _audioPlayer.pause();
-      setState(() => _isPlaying = false);
-    } else {
-      await _audioPlayer.play(UrlSource(_filePath));
-      setState(() => _isPlaying = true);
-    }
-  }
-
-  void _seekToSecond(int second) {
-    Duration newDuration = Duration(seconds: second);
-    _audioPlayer.seek(newDuration);
-  }
-
-  String formatPosition(int position) {
-    double result = position / 1000;
-    String minutes = (result / 60).floor().toString();
-    String secondes = (result % 60).floor().toString();
-    return minutes + ':' + secondes;
-  }
-
-  Future<void> pickAudioFile() async {
-    try {
-      FilePickerResult? result = await FilePicker.platform.pickFiles(
-        type: FileType.audio,
-      );
-
-      if (result != null) {
-        PlatformFile file = result.files.first;
-        setState(() {
-          _filePath = file.path!;
-          _audioPlayer.play(UrlSource(_filePath));
-        });
-      }
-    } catch (e) {
-      print("Error while picking the file: ${e.toString()}");
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        IconButton(
-          icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
-          onPressed: _playPause,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(formatPosition(_position.inMilliseconds)),
-            SizedBox(width: 5),
-            Slider(
-              activeColor: Color(0xFF6A1B9A),
-              inactiveColor: Color(0xFF6A1B9A).withOpacity(0.3),
-              min: 0.0,
-              max: _audioDuration.inSeconds.toDouble(),
-              value: _position.inSeconds.toDouble(),
-              onChanged: (double value) {
-                setState(() {
-                  _seekToSecond(value.toInt());
-                  value = value;
-                });
-              },
-            ),
-            SizedBox(width: 5),
-            Text(formatPosition(_audioDuration.inMilliseconds)),
-          ],
-        ),
-        ElevatedButton(
-            onPressed: () {
-              pickAudioFile();
-            },
-            child: Text('Pick a file')),
-      ],
-    );
-  }
-}
-
-class RecordSection extends StatefulWidget {
-  const RecordSection({super.key});
-
-  @override
-  State<RecordSection> createState() => _RecordSectionState();
-}
-
-class _RecordSectionState extends State<RecordSection> {
+class _ButtonSectionState extends State<ButtonSection> {
   FlutterSoundRecorder? audioRecorder;
-  FlutterSoundPlayer? _player;
-  String? audioFilePath = "";
-  late FlutterSound flutterSound;
-
-  void initState() {
-    super.initState();
-  }
+  FlutterSoundPlayer? player;
+  bool isRecording = false;
 
   Future<bool> checkPermission() async {
     var status = await Permission.microphone.request();
@@ -192,12 +70,19 @@ class _RecordSectionState extends State<RecordSection> {
         // Stop any ongoing recording before starting a new one
         await audioRecorder!.stopRecorder();
       }
-
+      File existingFile = File('audio.aac');
+      if (existingFile.existsSync()) {
+        await existingFile.delete();
+        print('Existing file deleted');
+      }
       audioRecorder = FlutterSoundRecorder();
 
       // Start recording audio
       await audioRecorder?.openRecorder();
-      await audioRecorder!.startRecorder(toFile: 'path_to_save_recording.aac');
+      await audioRecorder!.startRecorder(toFile: 'audio.aac');
+      setState(() {
+        isRecording = true;
+      });
     } catch (e) {
       print('Error starting recording: $e');
     }
@@ -211,39 +96,226 @@ class _RecordSectionState extends State<RecordSection> {
         await audioRecorder!.closeRecorder();
         audioRecorder = null;
         print("Recording stopped");
+        setState(() {
+          audioPath = 'audio.aac';
+          isRecording = false;
+        });
 
-        // Play the recorded audio
-        _player = FlutterSoundPlayer();
-
-        await _player!.openPlayer();
-        await _player!.startPlayer(fromURI: 'path_to_save_recording.aac');
-        _player!.setVolume(1.0); // Set volume to maximum (1.0)
+        String filePath = '/data/user/0/com.example.ilili/cache/audio.aac';
+        audioPath = filePath;
       }
+      ;
     } catch (e) {
       print('Error stopping recording or playing audio: $e');
     }
   }
 
+  Future<void> pickAudioFile() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.audio,
+      );
+
+      if (result != null) {
+        PlatformFile file = result.files.first;
+        print("File path: ${file.path}");
+        setState(() {
+          audioPath = file.path!;
+        });
+      }
+    } catch (e) {
+      print("Error while picking the file: ${e.toString()}");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        if (isRecording)
+          ElevatedButton(
+            onPressed: () {
+              stopRecording();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.stop),
+                SizedBox(width: 5),
+                Text('Stop Recording'),
+              ],
+            ),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              fixedSize:
+                  Size(150, 30), // Set the width and height of the button
+              backgroundColor:
+                  Color(0xFF6A1B9A), // Set the background color of the button
+            ),
+          ),
+        if (!isRecording)
+          ElevatedButton(
+            onPressed: () {
+              startRecording();
+            },
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Icon(Icons.mic),
+              SizedBox(width: 5),
+              Text('Record Audio')
+            ]),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              fixedSize:
+                  Size(150, 30), // Set the width and height of the button
+              backgroundColor:
+                  Color(0xFF6A1B9A), // Set the background color of the button
+            ),
+          ),
+        SizedBox(width: 20),
         ElevatedButton(
           onPressed: () {
-            startRecording();
+            pickAudioFile();
           },
-          child: Text('Start Recording'),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Icon(Icons.audiotrack),
+            SizedBox(width: 5),
+            Text('Pick Audio')
+          ]),
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            fixedSize: Size(150, 30), // Set the width and height of the button
+            backgroundColor:
+                Color(0xFF6A1B9A), // Set the background color of the button
+          ),
         ),
-        ElevatedButton(
-          onPressed: () {
-            stopRecording();
-          },
-          child: Text('Stop Recording'),
+      ],
+    );
+  }
+}
+
+class AudioPlayerSection extends StatefulWidget {
+  AudioPlayerSection({super.key});
+
+  @override
+  State<AudioPlayerSection> createState() => AudioPlayerSectionState();
+}
+
+class AudioPlayerSectionState extends State<AudioPlayerSection> {
+  bool isPlaying = false;
+  Duration audioDuration = Duration();
+  Duration position = Duration();
+  String error = '';
+
+  void initState() {
+    super.initState();
+    audioPlayer.onDurationChanged.listen((Duration duration) {
+      setState(() {
+        audioDuration = duration;
+      });
+    });
+
+    audioPlayer.onPositionChanged.listen((Duration pos) {
+      setState(() {
+        position = pos;
+      });
+    });
+  }
+
+  void playPause() async {
+    try {
+      if (isPlaying) {
+        await audioPlayer.pause();
+        setState(() => isPlaying = false);
+      } else {
+        if (audioPath != null) {
+          print(audioPath);
+          await audioPlayer.play(UrlSource(audioPath));
+          setState(() => isPlaying = true);
+        }
+      }
+    } catch (e) {
+      setState(() {
+        error = "Please select a valid audio file";
+      });
+    }
+  }
+
+  void _seekToSecond(int second) {
+    Duration newDuration = Duration(seconds: second);
+    audioPlayer.seek(newDuration);
+  }
+
+  String formatPosition(int position) {
+    double result = position / 1000;
+    String minutes = (result / 60).floor().toString();
+    String secondes = (result % 60).floor().toString();
+    return minutes + ':' + secondes;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        if (error != '')
+          Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.error, color: Colors.red),
+                SizedBox(width: 5),
+                Container(
+                  width: 250,
+                  child: Text(
+                    "${error}",
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+              onPressed: () {
+                playPause();
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(formatPosition(position.inMilliseconds)),
+                Slider(
+                  activeColor: Color(0xFF6A1B9A),
+                  inactiveColor: Color(0xFF6A1B9A).withOpacity(0.3),
+                  min: 0.0,
+                  max: audioDuration.inSeconds.toDouble(),
+                  value: position.inSeconds.toDouble(),
+                  onChanged: (double value) {
+                    setState(() {
+                      _seekToSecond(value.toInt());
+                      value = value;
+                    });
+                  },
+                ),
+                Text(formatPosition(audioDuration.inMilliseconds)),
+              ],
+            )
+          ],
         )
       ],
-    ));
+    );
   }
 }
 
@@ -286,63 +358,73 @@ class _TagsSectionState extends State<TagsSection> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if (error != "")
-          Padding(
-            padding: EdgeInsets.only(bottom: 10),
-            child: Row(
-              children: [
-                Icon(Icons.error, color: Colors.red),
-                SizedBox(width: 5),
-                Container(
-                  width: 250,
-                  child: Text(
-                    "${error}",
-                    style: TextStyle(
-                        color: Colors.red, fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
+    return Container(
+      width: 300,
+      child: Column(
+        children: [
+          if (error != "")
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Icon(Icons.error, color: Colors.red),
+                  SizedBox(width: 5),
+                  Container(
+                    width: 250,
+                    child: Text(
+                      "${error}",
+                      style: TextStyle(
+                          color: Colors.red, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          TextField(
+            controller: tagController,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              suffixIcon: IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  addTag();
+                },
+              ),
+              labelText: 'tag',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
             ),
           ),
-        TextField(
-          controller: tagController,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-            suffixIcon: IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                addTag();
-              },
-            ),
-            labelText: 'tag',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: tagsList.length,
+            itemBuilder: (context, index) {
+              return Container(
+                width: 200, // Set the desired width here
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(tagsList[index]),
+                    IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          tagsList.removeAt(index);
+                        });
+                      },
+                    )
+                  ],
+                ),
+              );
+            },
           ),
-        ),
-        ListView.builder(
-          shrinkWrap: true,
-          itemCount: tagsList.length,
-          itemBuilder: (context, index) {
-            return Row(
-              children: [
-                Text(tagsList[index]),
-                IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    setState(() {
-                      tagsList.removeAt(index);
-                    });
-                  },
-                )
-              ],
-            );
-          },
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
