37fcf64ad5ead5189e427f3318534b3738e207fa
lahoucine.lapunaire <lahoucine.lapunaire@ensiie.fr>
2023-07-07 12:46:38 +0200

    [FEAT] ChangeProfile : get profile photo, pick up storage image

--- lib/components/changeProfile.dart
+++ lib/components/changeProfile.dart
@@ -8,7 +8,8 @@ FirebaseAuth auth = FirebaseAuth.instance;
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 String username = "";
-String profilePicture = "";
+String profilePicture =
+    "https://firebasestorage.googleapis.com/v0/b/ilili-7ebc6.appspot.com/o/users%2Fuser-default.jpg?alt=media&token=8aa7825f-2890-4f63-9fb2-e66e7e916256";
 String description = "";
 bool isPhotoChanged = false;
 
@@ -23,6 +24,7 @@ class ChangeProfilePage extends StatelessWidget {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          ProfilPictureSection(),
           Text("Change Profile Page"),
         ],
       )),
@@ -38,18 +40,32 @@ class ProfilPictureSection extends StatefulWidget {
 }
 
 class _ProfilPictureSectionState extends State<ProfilPictureSection> {
-  File? _image;
+  File? image;
   final picker = ImagePicker();
 
-  void getProfilePhoto() async {}
+  @override
+  void initState() {
+    super.initState();
+    getProfilePhoto();
+  }
 
-  Future getImage() async {
+  void getProfilePhoto() async {
+    DocumentSnapshot ds =
+        await firestore.collection('users').doc(auth.currentUser!.uid).get();
+
+    setState(() {
+      profilePicture = ds.get('profilPicture');
+      print("profilePicture: " + profilePicture);
+    });
+  }
+
+  void pickImage() async {
     final XFile? pickedFile =
         await picker.pickImage(source: ImageSource.gallery);
 
     setState(() {
       if (pickedFile != null) {
-        _image = File(pickedFile.path);
+        image = File(pickedFile.path);
       } else {
         print('No image selected.');
       }
@@ -58,6 +74,29 @@ class _ProfilPictureSectionState extends State<ProfilPictureSection> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: () {
+        pickImage();
+      },
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(50),
+          child: image == null
+              ? Image.network(
+                  profilePicture,
+                  fit: BoxFit.cover,
+                )
+              : Image.file(
+                  image!,
+                  fit: BoxFit.cover,
+                ),
+        ),
+      ),
+    );
   }
 }
